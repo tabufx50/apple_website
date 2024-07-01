1f358105a03e1cd0e808645303267d9eaa8a1f3e
tabufx50 <tabufx50@hotmail.com>
2024-06-30 17:26:07 -0700

    added new components

---
+++ src/components/Features.jsx
@@ -0,0 +1,29 @@
+import { useGSAP } from '@gsap/react'
+import React from 'react'
+import { animateWithGsap } from '../utils/animations'
+
+const Features = () => {
+  useGSAP(() => {
+animateWithGsap('#features_title', {y:0, opacity: 1})
+
+  }, [])
+
+  return (
+    <section className='h-full common-padding bg-zinc relative overflow-hidden'>
+      <div className='screen-max-width'>
+          <div className='mb-12 w-full'>
+            <h1 id="features_title" className='section-heading'>Explore the full story.</h1>
+          </div>
+
+          <div className='flex flex-col justify-center items-center overflow-hidden'>
+            <div className='mt-32 mb-24 pl-24'>
+              <h2 className='text-5xl lg:text-7xl'>iPhone.</h2>
+            </div>
+
+          </div> 
+      </div>
+    Features</section>
+  )
+}
+
+export default Features
